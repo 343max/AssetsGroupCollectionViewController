ad610f844acd535c20a43274c97456d6b37c43b1
Max Winde <max.winde@gmail.com>
2014-02-01 16:56:11 +0100

    Initial Commit

---
+++ AssetsGroupCollectionViewController/DAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DAAppDelegate.h
+//  AssetsGroupCollectionViewController
+//
+//  Created by Max Winde on 01.02.14.
+//  Copyright (c) 2014 Max von Webel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AssetsGroupCollectionViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AssetsGroupCollectionViewController
+//
+//  Created by Max Winde on 01.02.14.
+//  Copyright (c) 2014 Max von Webel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DAAppDelegate class]));
+    }
+}
